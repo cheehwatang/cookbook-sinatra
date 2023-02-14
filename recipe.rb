84354442b7161b301d160ed05e27d734d6ec85e0
Chee Hwa Tang <cheehwa.tang@gmail.com>
2023-02-14 09:24:04 +0800

    Refactored the Recipe class to take in default values from the hash attributes

--- recipe.rb
+++ recipe.rb
@@ -1,12 +1,12 @@
 class Recipe
   attr_reader :name, :description, :rating, :prep_time
 
-  def initialize(recipe_details)
-    @name = recipe_details[:name]
-    @description = recipe_details[:description]
-    @rating = recipe_details[:rating]
-    @prep_time = recipe_details[:prep_time]
-    @done = recipe_details.key?(:done) ? recipe_details[:done] : false
+  def initialize(attributes = {})
+    @name = attributes[:name] || ""
+    @description = attributes[:description] || ""
+    @rating = attributes[:rating] || 0
+    @prep_time = attributes[:prep_time] || "0 mins"
+    @done = attributes[:done] || false
   end
 
   def done?
